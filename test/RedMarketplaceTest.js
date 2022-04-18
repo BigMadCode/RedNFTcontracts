a6daaf0f2d0e3f03e37673d283cc7d1d19990097
leonatwork <noelabydas@gmail.com>
2022-04-18 21:40:54 +0530

    tokenId fix

--- test/RedMarketplaceTest.js
+++ test/RedMarketplaceTest.js
@@ -1,91 +1,102 @@
-const Marketplace = artifacts.require('RedMarketplace');
-const Token = artifacts.require('Red');
-const NFTContract = artifacts.require('RedNFT');
+const Marketplace = artifacts.require("RedMarketplace");
+const Token = artifacts.require("Red");
+const NFTContract = artifacts.require("RedNFT");
 
-const chai = require('./setupchai.js');
+const chai = require("./setupchai.js");
 const BN = web3.utils.BN;
 const expect = chai.expect;
 
-require('dotenv').config({ path: '../.env' });
+require("dotenv").config({ path: "../.env" });
 
-contract('Marketplace Test', function (accounts) {
-	const [deployerAccount, recipient, anotherAccount] = accounts;
+contract("Marketplace Test", function (accounts) {
+  const [deployerAccount, recipient, anotherAccount] = accounts;
 
-	beforeEach(async () => {
-		this.redMarketplace = await Marketplace.deployed(123); //new(process.env.RED_TOKEN);
-	});
+  beforeEach(async () => {
+    this.redMarketplace = await Marketplace.deployed(123); //new(process.env.RED_TOKEN);
+  });
 
-	it(' #1 Make sure contract deployed correctly', async () => {
-		let instance = await this.redMarketplace;
-		let contractAddress = instance.address;
-		console.log(contractAddress.toString());
-		expect(instance.address).not.equal(undefined);
-	});
+  it(" #1 Make sure contract deployed correctly", async () => {
+    let instance = await this.redMarketplace;
+    let contractAddress = instance.address;
+    console.log(contractAddress.toString());
+    expect(instance.address).not.equal(undefined);
+  });
 
-	it('#2 should list token lisitngs', async () => {
-		let instance = this.redMarketplace;
-		let nftInstance = await NFTContract.deployed();
-		let balanceRecipient = await nftInstance.balanceOf(recipient);
-		console.log(balanceRecipient.toString() + ' this is balance of Recipient');
-		let redInstance = await Token.deployed();
-		let askingPrice = 10;
-		let NFT = await expect(nftInstance.safeMint(recipient, 'www.redeyedcollection.com/')).to.eventually.be
-			.fulfilled;
-		let nftTokenId = await NFT;
-		//console.log(NFT);
-		let balanceRecipient1 = await nftInstance.balanceOf(recipient);
-		//console.log(nftTokenId.toString());
-		console.log(balanceRecipient1.toString() + ' this is balance of Recipient1');
-		expect(instance.listItem(NFT.tokenId, NFT.address, askingPrice, 1, recipient.address)).to.eventually.be
-			.fulfilled;
-	});
+  it("#2 should list token lisitngs", async () => {
+    let instance = this.redMarketplace;
+    let nftInstance = await NFTContract.deployed();
+    let balanceRecipient = await nftInstance.balanceOf(recipient);
+    console.log(balanceRecipient.toString() + " this is balance of Recipient");
+    let redInstance = await Token.deployed();
+    let askingPrice = 10;
+    let NFT = await expect(
+      nftInstance.safeMint(recipient, "www.redeyedcollection.com/")
+    ).to.eventually.be.fulfilled;
+    let nftTokenId = await NFT.logs[0].args.tokenId.toString();
+    //console.log(NFT);
+    let balanceRecipient1 = await nftInstance.balanceOf(recipient);
+    console.log(nftTokenId);
+    console.log(NFT.address);
+    console.log(
+      balanceRecipient1.toString() + " this is balance of Recipient1"
+    );
+    expect(
+      instance.listItem(
+        nftTokenId,
+        NFT.address,
+        askingPrice,
+        1,
+        recipient.address
+      )
+    ).to.eventually.be.fulfilled;
+  });
 
-	// it('#3 it is possible to make an offer', async () => {
-	// 	let instance = this.redMarketplace;
-	// 	let balanceOfDeployer = await instance.balanceOf(deployerAccount);
+  // it('#3 it is possible to make an offer', async () => {
+  // 	let instance = this.redMarketplace;
+  // 	let balanceOfDeployer = await instance.balanceOf(deployerAccount);
 
-	// 	expect(instance.transfer(recipient, new BN(balanceOfDeployer + 1))).to.eventually.be.rejected;
+  // 	expect(instance.transfer(recipient, new BN(balanceOfDeployer + 1))).to.eventually.be.rejected;
 
-	// 	expect(instance.balanceOf(deployerAccount)).to.eventually.be.a.bignumber.equal(balanceOfDeployer);
-	// });
+  // 	expect(instance.balanceOf(deployerAccount)).to.eventually.be.a.bignumber.equal(balanceOfDeployer);
+  // });
 
-	// it('#4 it is possible to cancel offer', async () => {
-	// 	let instance = this.myToken;
-	// 	await expect(instance.increaseAllowance(recipient, 1000000000, { from: deployerAccount })).to.eventually.be
-	// 		.fulfilled;
-	// 	let allowances = await instance.allowance.call(deployerAccount, recipient);
-	// 	return console.log(allowances.toString());
-	// });
+  // it('#4 it is possible to cancel offer', async () => {
+  // 	let instance = this.myToken;
+  // 	await expect(instance.increaseAllowance(recipient, 1000000000, { from: deployerAccount })).to.eventually.be
+  // 		.fulfilled;
+  // 	let allowances = await instance.allowance.call(deployerAccount, recipient);
+  // 	return console.log(allowances.toString());
+  // });
 
-	// it('#5 it is possible to buy directly', async () => {
-	// 	const sendTokens = 1000000000;
-	// 	let instance = this.myToken;
-	// 	let balanceR = await instance.balanceOf(recipient);
-	// 	console.log(balanceR.toString());
-	// 	let totalSupply = await instance.totalSupply();
-	// 	expect(instance.balanceOf(deployerAccount)).to.eventually.be.a.bignumber.equal(totalSupply);
-	// 	await expect(instance.transfer(recipient, sendTokens)).to.eventually.be.fulfilled;
-	// 	let balanceRA = await instance.balanceOf(recipient);
-	// 	console.log(balanceRA.toString());
-	// 	expect(instance.balanceOf(deployerAccount)).to.eventually.be.a.bignumber.equal(
-	// 		totalSupply.sub(new BN(sendTokens)),
-	// 	);
-	// 	expect(instance.balanceOf(recipient)).to.eventually.be.a.bignumber.equal(new BN(sendTokens));
-	// });
+  // it('#5 it is possible to buy directly', async () => {
+  // 	const sendTokens = 1000000000;
+  // 	let instance = this.myToken;
+  // 	let balanceR = await instance.balanceOf(recipient);
+  // 	console.log(balanceR.toString());
+  // 	let totalSupply = await instance.totalSupply();
+  // 	expect(instance.balanceOf(deployerAccount)).to.eventually.be.a.bignumber.equal(totalSupply);
+  // 	await expect(instance.transfer(recipient, sendTokens)).to.eventually.be.fulfilled;
+  // 	let balanceRA = await instance.balanceOf(recipient);
+  // 	console.log(balanceRA.toString());
+  // 	expect(instance.balanceOf(deployerAccount)).to.eventually.be.a.bignumber.equal(
+  // 		totalSupply.sub(new BN(sendTokens)),
+  // 	);
+  // 	expect(instance.balanceOf(recipient)).to.eventually.be.a.bignumber.equal(new BN(sendTokens));
+  // });
 
-	// it('#6 it is possible to update listing price', async () => {
-	// 	const sendTokens = 1000000000;
-	// 	let instance = this.myToken;
-	// 	let balanceR = await instance.balanceOf(recipient);
-	// 	console.log(balanceR.toString());
-	// 	let totalSupply = await instance.totalSupply();
-	// 	expect(instance.balanceOf(deployerAccount)).to.eventually.be.a.bignumber.equal(totalSupply);
-	// 	await expect(instance.transfer(recipient, sendTokens)).to.eventually.be.fulfilled;
-	// 	let balanceRA = await instance.balanceOf(recipient);
-	// 	console.log(balanceRA.toString());
-	// 	expect(instance.balanceOf(deployerAccount)).to.eventually.be.a.bignumber.equal(
-	// 		totalSupply.sub(new BN(sendTokens)),
-	// 	);
-	// 	expect(instance.balanceOf(recipient)).to.eventually.be.a.bignumber.equal(new BN(sendTokens));
-	// });
+  // it('#6 it is possible to update listing price', async () => {
+  // 	const sendTokens = 1000000000;
+  // 	let instance = this.myToken;
+  // 	let balanceR = await instance.balanceOf(recipient);
+  // 	console.log(balanceR.toString());
+  // 	let totalSupply = await instance.totalSupply();
+  // 	expect(instance.balanceOf(deployerAccount)).to.eventually.be.a.bignumber.equal(totalSupply);
+  // 	await expect(instance.transfer(recipient, sendTokens)).to.eventually.be.fulfilled;
+  // 	let balanceRA = await instance.balanceOf(recipient);
+  // 	console.log(balanceRA.toString());
+  // 	expect(instance.balanceOf(deployerAccount)).to.eventually.be.a.bignumber.equal(
+  // 		totalSupply.sub(new BN(sendTokens)),
+  // 	);
+  // 	expect(instance.balanceOf(recipient)).to.eventually.be.a.bignumber.equal(new BN(sendTokens));
+  // });
 });
